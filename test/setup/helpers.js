8327118f1811ee384edf5e712d9054049e40b0b4
Jmeas <jellyes2@gmail.com>
2014-07-25 00:15:55 -0400

    Adds grunt build and test setup.

---
+++ test/setup/helpers.js
@@ -0,0 +1,34 @@
+function setupTestHelpers() {
+  beforeEach(function() {
+    this.sinon = sinon.sandbox.create();
+    global.stub = _.bind(this.sinon.stub, this.sinon);
+    global.spy  = _.bind(this.sinon.spy, this.sinon);
+  });
+
+  afterEach(function() {
+    this.sinon.restore();
+    delete global.stub;
+    delete global.spy;
+  });
+}
+
+// when running in node
+if (typeof exports !== 'undefined') {
+  setupTestHelpers();
+}
+
+// when running in browser
+else {
+  this.global = window;
+  mocha.setup('bdd');
+
+  window.expect = chai.expect;
+  window.sinon = sinon;
+
+  onload = function() {
+    mocha.checkLeaks();
+    mocha.globals(['stub', 'spy', 'document']);
+    mocha.run();
+    setupTestHelpers();
+  };
+}
---
+++ test/setup/node.js
@@ -0,0 +1,29 @@
+if (!global.document || !global.window) {
+  var jsdom = require('jsdom').jsdom;
+
+  global.document = jsdom('<html><head><script></script></head><body></body></html>', null, {
+    FetchExternalResources   : ['script'],
+    ProcessExternalResources : ['script'],
+    MutationEvents           : '2.0',
+    QuerySelector            : false
+  });
+
+  global.window = document.createWindow();
+  global.navigator = global.window.navigator;
+}
+
+var sinon = require('sinon');
+var chai = require('chai');
+var sinonChai = require('sinon-chai');
+
+global._ = require('underscore');
+global.Backbone = require('backbone');
+global.$ = global.jQuery = Backbone.$ = require('jquery');
+global.Marionette = require('backbone.marionette');
+
+chai.use(sinonChai);
+
+global.expect = chai.expect;
+global.sinon = sinon;
+
+require('../../marionette.transition-region.js');
